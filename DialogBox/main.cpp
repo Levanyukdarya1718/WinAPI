5c92c24dbd9e305f7bbc2adf967c97fd8c26b643
Дашка Леванюк <levanyuk.darja@yandex.ru>
2024-10-29 22:58:39 +0500

    cOPYdone

--- DialogBox/main.cpp
+++ DialogBox/main.cpp
@@ -20,6 +20,19 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_COMMAND:
 		switch (LOWORD(wParam) )
 		{
+		case IDC_BUTTON_COPY:
+		{
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			//ФУНКЦИЯ GetDlgItem() возвращает HWND дочернего элемента окна по его ResourceID.
+			//HWND - Heander to Window (Дискриптор окна) - это число при помощи которого можно обратиться к окну.
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE];
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			
+		}
+		break;
 		case IDOK: MessageBox(hwnd, "Была нажата кнопка OK!", "Info", MB_OK | MB_ICONINFORMATION); break;
 		case IDCANCEL: EndDialog(hwnd, 0); break;
 		}
--- DialogBox/resource.h
+++ DialogBox/resource.h
@@ -4,6 +4,12 @@
 //
 #define IDD_DIALOG1                     101
 #define IDI_ICON2                       104
+#define IDC_STATIC_                     1001
+#define IDC_EDIT_LOGIN                  1002
+#define IDC_EDIT_PASSWORD               1004
+#define IDC_STATIC2                     1005
+#define IDC_BUTTON1                     1006
+#define IDC_BUTTON_COPY                 1006
 
 // Next default values for new objects
 // 
@@ -11,7 +17,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
