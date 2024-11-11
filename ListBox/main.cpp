8a979f364d10ffd72a7581a04e1ac831d12a2437
Дашка Леванюк <levanyuk.darja@yandex.ru>
2024-11-12 00:57:59 +0500

    MainWindowStarted

--- ListBox/main.cpp
+++ ListBox/main.cpp
@@ -6,9 +6,8 @@
 CONST CHAR* g_VALUES[] = { "This", "is", "my", "first", "List", "box" };
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-
 BOOL CALLBACK DlgProcAddItem(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-
+BOOL CALLBACK DlgProcEditItem(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -36,9 +35,24 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
+		case IDC_LIST1:
+			switch (HIWORD(wParam))
+			{
+			case LBN_DBLCLK:
+				DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, DlgProcEditItem, 0);
+				break;
+			}
+			break;
 		case IDC_BUTTON_ADD:
 			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, DlgProcAddItem, 0);
 			break;
+		case IDC_BUTTON_DELETE:
+		{
+			HWND hListBox = GetDlgItem(hwnd, IDC_LIST1);
+			INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+			SendMessage(hListBox, LB_DELETESTRING, i, 0);
+		}
+		break;
 		case IDOK:
 		{
 			CONST INT SIZE = 256;
@@ -51,6 +65,14 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
 		}
 			break;
+		case WM_KEYDOWN:
+			switch (wParam)
+			{
+			case VK_DELETE:
+				MessageBox(hwnd, "Delete pressed", "Info", MB_OK | MB_ICONINFORMATION);
+				SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_DELETE), (LPARAM)GetDlgItem(hwnd, IDC_LIST1));
+				break;
+			}
 		case IDCANCEL:
 			EndDialog(hwnd, 0);
 			break;
@@ -79,6 +101,7 @@ BOOL CALLBACK DlgProcAddItem(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			CHAR sz_buffer[SIZE]{};
 			HWND hEditAdd = GetDlgItem(hwnd, IDC_EDIT_ADD);
 			SendMessage(hEditAdd, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			if (strlen(sz_buffer) == 0)break;
 
 			HWND hList = GetDlgItem(GetParent(hwnd), IDC_LIST1);
 			//GetParent (hwnd) возвращает родительское токно (HWND) указанного окна для укакзанногог окна
@@ -93,7 +116,6 @@ BOOL CALLBACK DlgProcAddItem(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 				 if (result == IDYES)break;
 			}
 		}
-	
 		case IDCANCEL:
 			EndDialog(hwnd, 0);
 			break;
@@ -104,4 +126,44 @@ BOOL CALLBACK DlgProcAddItem(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		break;
 	}
 	return	FALSE;
+}
+BOOL CALLBACK DlgProcEditItem(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)"Edit item");
+		HWND hParent = GetParent(hwnd);
+		HWND hListBox = GetDlgItem(hParent, IDC_LIST1);
+		HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_ADD);
+		CONST INT SIZE = 256;
+		CHAR sz_buffer[SIZE]{};
+		INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+		SendMessage(hListBox, LB_GETTEXT, i, (LPARAM)sz_buffer);
+		SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+	}
+	break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			HWND hParent = GetParent(hwnd);
+			HWND hListBox = GetDlgItem(hParent, IDC_LIST1);
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_ADD);
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE];
+			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+			SendMessage(hListBox, LB_DELETESTRING, i, 0);
+			SendMessage(hListBox, LB_INSERTSTRING, i, (LPARAM)sz_buffer);
+		}
+		case IDCANCEL:EndDialog(hwnd, 0); break;
+		}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+	}
+	return FALSE;
 }
---
+++ MainWindows/main.cpp
@@ -0,0 +1,85 @@
+//MainWindow
+#include <Windows.h>
+
+CONST CHAR g_sz_MY_WINDOW_CLASS[] = "My Window"; //Имя Класса окна
+
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	//1)Регистрация класса окна:
+	WNDCLASSEX wc; //windows class
+	ZeroMemory(&wc, sizeof(wc));//обнуляыем экземпляр
+
+	//связаны с размером и стилем
+	wc.style = 0;
+	wc.cbSize = sizeof(wc);
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0; //cb... - Count Bytes
+
+	//связаны с внешним видом
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hIconSm=LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+
+	//
+	wc.hInstance = hInstance;
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = g_sz_MY_WINDOW_CLASS;
+	wc.lpfnWndProc = (WNDPROC)WndProc;
+
+	//регистрация функции
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, "Class registration failed", NULL, MB_OK | MB_ICONERROR);
+		return 0;
+	}
+	// 2) Созхдание окна:
+	HWND hwnd = CreateWindowEx
+	(
+		NULL, //ExStyles
+		g_sz_MY_WINDOW_CLASS, //Class name
+		g_sz_MY_WINDOW_CLASS, //Window title
+		WS_OVERLAPPEDWINDOW, //Window style
+		CW_USEDEFAULT, CW_USEDEFAULT, //Window position
+		CW_USEDEFAULT, CW_USEDEFAULT, //Window Size
+		NULL, //Parent Window
+		NULL, //Main menu Resourceid for MainWindow or ResourceID for ChildWondow
+		hInstance,
+		NULL
+
+	);
+	ShowWindow(hwnd, nCmdShow);//Задает режим отображения окна
+								//(Развернуто на весь экран, свернутто в окно, свернуто на панель задач)
+	UpdateWindow(hwnd); //Прорисовывает окно
+
+	//3) Запуск цикла сообщений:
+	MSG msg; //Создаем сообщение
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+
+	}
+	return msg.message;
+}
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CREATE:
+		break;
+	case WM_COMMAND:
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+	default: return DefWindowProc(hwnd, uMsg, wParam, lParam);
+
+	}
+	return FALSE;
+}
